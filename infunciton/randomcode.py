c847a215c0d8f78f1b227a746be136ad17b12070
JenyYang <yrjword@163.com>
2017-09-25 11:10:54 +0800

    获取随机验证码

---
+++ infunciton/randomcode.py
@@ -0,0 +1,19 @@
+#! /user/bin/python/
+# -*-coding:utf-8 -*-
+import random
+#获取一个任意位数的随机验证码
+def getrandomcode(args):
+    temp=""
+    for i in range(args):
+        rad1=random.randrange(0,4)
+        if rad1/2==0:
+            temp=temp+str(rad1)
+        else:
+            rad2=random.randrange(65,91)#获取随机大写字母
+            c=chr(rad2)
+            temp=temp+c
+    return temp
+randomcode=getrandomcode(2)
+print(randomcode)
+
+
